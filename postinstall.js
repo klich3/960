672d67cd69bb7174f9db2e0afd52138a342a160c
Anton Sychev <anton@sychev.xyz>
2024-09-08 15:06:23 +0200

    added: post install script show message

---
+++ postinstall.js
@@ -0,0 +1,15 @@
+/*
+█▀ █▄█ █▀▀ █░█ █▀▀ █░█
+▄█ ░█░ █▄▄ █▀█ ██▄ ▀▄▀
+
+Author: <Anton Sychev> (anton at sychev dot xyz)
+postinstall.js (c) 2024
+Created:  2024-09-08 21:01:09 
+Desc: Message after installation
+*/
+
+console.log(
+	"T960.css installed.\n" +
+		"> Run  `npm i -D sass-embedded`.\n" +
+		'> add `import "960.css";` in your main.js'
+);
